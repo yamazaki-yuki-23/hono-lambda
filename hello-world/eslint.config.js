8d6a46b9be89d632df3f579e5091a020fe34bd75
yamazaki-yuki-23 <natural4083@gmail.com>
2025-11-09 02:17:52 +0900

    Refactor code structure for improved readability and maintainability

--- hello-world/eslint.config.js
+++ hello-world/eslint.config.js
@@ -1,10 +1,24 @@
 import tseslint from "@typescript-eslint/eslint-plugin";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
+import { importX } from "eslint-plugin-import-x";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+
+const tsResolver = createTypeScriptImportResolver({
+  project: ["./tsconfig.json", "./tsconfig.build.json"],
+  noWarnOnMultipleProjects: true,
+});
 
 export default [
   {
-    ignores: ["**/*.js", "node_modules", ".aws-sam"] 
+    ignores: ["**/*.js", "node_modules", ".aws-sam"],
   },
   ...tseslint.configs["flat/recommended"],
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    settings: {
+      "import-x/resolver-next": [tsResolver],
+    },
+  },
   prettierRecommended,
 ];
